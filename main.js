d27c8dbabf65b8a2015c80fd8af87e9e6bde4663
Bregina Pool <swimmingturtle@hotmail.com>
2022-07-11 20:58:54 +1200

    Remove mobile functionality

--- main.js
+++ main.js
@@ -23,15 +23,6 @@ canvas.addEventListener('mousedown', (e) => {
 canvas.addEventListener('mouseup', () => (isDrawing = false))
 canvas.addEventListener('mouseout', () => (isDrawing = false))
 
-// for touch screens
-canvas.addEventListener('touchmove', draw)
-canvas.addEventListener('touchdown', (e) => {
-  isDrawing = true
-  lastX = e.offsetX
-  lastY = e.offsetY
-})
-canvas.addEventListener('touchup', () => (isDrawing = false))
-
 function draw(e) {
   if (!isDrawing) return
   ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`
@@ -67,76 +58,3 @@ document.querySelector('.btn-erase').onclick = function () {
 document.querySelector('.btn-xor').onclick = function () {
   ctx.globalCompositeOperation = 'xor'
 }
-
-// Set up touch events for mobile, etc
-canvas.addEventListener(
-  'touchstart',
-  function (e) {
-    mousePos = getTouchPos(canvas, e)
-    var touch = e.touches[0]
-    var mouseEvent = new MouseEvent('mousedown', {
-      clientX: touch.clientX,
-      clientY: touch.clientY,
-    })
-    canvas.dispatchEvent(mouseEvent)
-  },
-  false
-)
-canvas.addEventListener(
-  'touchend',
-  function (e) {
-    var mouseEvent = new MouseEvent('mouseup', {})
-    canvas.dispatchEvent(mouseEvent)
-  },
-  false
-)
-canvas.addEventListener(
-  'touchmove',
-  function (e) {
-    var touch = e.touches[0]
-    var mouseEvent = new MouseEvent('mousemove', {
-      clientX: touch.clientX,
-      clientY: touch.clientY,
-    })
-    canvas.dispatchEvent(mouseEvent)
-  },
-  false
-)
-
-// Get the position of a touch relative to the canvas
-function getTouchPos(canvasDom, touchEvent) {
-  var rect = canvasDom.getBoundingClientRect()
-  return {
-    x: touchEvent.touches[0].clientX - rect.left,
-    y: touchEvent.touches[0].clientY - rect.top,
-  }
-}
-
-// Prevent scrolling when touching the canvas
-document.body.addEventListener(
-  'touchstart',
-  function (e) {
-    if (e.target == canvas) {
-      e.preventDefault()
-    }
-  },
-  false
-)
-document.body.addEventListener(
-  'touchend',
-  function (e) {
-    if (e.target == canvas) {
-      e.preventDefault()
-    }
-  },
-  false
-)
-document.body.addEventListener(
-  'touchmove',
-  function (e) {
-    if (e.target == canvas) {
-      e.preventDefault()
-    }
-  },
-  false
-)
